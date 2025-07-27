faeec5fce7b090d85ba559bdb75bda310f5f089d
b-istiak-s <istiakshovon001@gmail.com>
2025-07-27 19:07:53 +0600

    remove relations from chat message output - unnecessary

--- src/chat-message/dto/chat-message.output.ts
+++ src/chat-message/dto/chat-message.output.ts
@@ -1,18 +1,9 @@
 import { Field, Int, ObjectType } from '@nestjs/graphql';
-import { Chat } from 'src/chat/chat.entity';
-import { ChatOutput } from 'src/chat/dto/chat.output';
-import { UserOutput } from 'src/user/dto/user.output';
 
 @ObjectType()
 export class ChatMessageOutput {
   @Field(() => Int)
   id: number;
-
-  @Field(() => ChatOutput)
-  chat: ChatOutput;
-
-  @Field(() => UserOutput)
-  sender: UserOutput;
 
   @Field(() => String)
   message: String;
