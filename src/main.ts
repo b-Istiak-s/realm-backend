f49844376d86a3c831ac046bbd161e239d43ca40
b-istiak-s <istiakshovon001@gmail.com>
2025-07-27 16:56:52 +0600

    set CORS properly to support multipart

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import graphqlUploadExpress from 'graphql-upload/graphqlUploadExpress.mjs';
 import * as dotenv from 'dotenv';
 dotenv.config();
 
@@ -7,9 +8,11 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: '*', // Allow any origin (only use this in dev)
+    origin: process.env.FRONTEND_URL,
+    credentials: true,
   });
+  app.use(graphqlUploadExpress({ maxFileSize: 10000000, maxFiles: 10 }));
 
-  await app.listen(process.env.PORT ?? 3002);
+  await app.listen(process.env.PORT ?? 40000);
 }
 bootstrap();
